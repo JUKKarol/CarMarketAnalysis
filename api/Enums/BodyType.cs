34dc028c11882e2342678e9a87ef336df234af13
JUKKarol <karolprzybylak2@gmail.com>
2024-01-13 12:58:54 +0100

    Add enums for car

---
+++ api/Enums/BodyType.cs
@@ -0,0 +1,16 @@
+﻿namespace CarMarketAnalysis.Enums
+{
+    public enum BodyType
+    {
+        None = 0,
+        SmallCar = 1,
+        UrbanCar = 2,
+        Coupe = 3,
+        Cabriolet = 4,
+        Combi = 5,
+        Compact = 6,
+        Minivan = 7,
+        Sedan = 8,
+        SUV = 9,
+    }
+}
---
+++ api/Enums/Currnecy.cs
@@ -0,0 +1,10 @@
+﻿namespace CarMarketAnalysis.Enums
+{
+    public enum Currnecy
+    {
+        None = 0,
+        PLN = 1,
+        EUR = 2,
+        USD = 3,
+    }
+}
