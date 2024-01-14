3c0fdd8e4a3439078c98018015c62d09327ffc96
JUKKarol <karolprzybylak2@gmail.com>
2024-01-14 12:55:17 +0100

    Add model repository

---
+++ api/Data/Repositories/ModelRepository/IModelRepository.cs
@@ -0,0 +1,14 @@
+﻿using CarMarketAnalysis.Entities;
+using Sieve.Models;
+
+namespace CarMarketAnalysis.Data.Repositories.ModelRepository
+{
+    public interface IModelRepository
+    {
+        Task<Model> GetModelById(Guid modelId);
+        Task<List<Model>> GetModels(SieveModel query);
+        Task<int> GetModelsCount(SieveModel query);
+        Task<Model> CreateModel(Model model);
+        Task<Model> UpdateModel(Model updatedModel);
+    }
+}
---
+++ api/Data/Repositories/ModelRepository/ModelRepository.cs
@@ -0,0 +1,65 @@
+﻿using CarMarketAnalysis.Entities;
+using Microsoft.EntityFrameworkCore;
+using Sieve.Models;
+using Sieve.Services;
+
+namespace CarMarketAnalysis.Data.Repositories.ModelRepository
+{
+    public class ModelRepository(
+        DatabaseContext db,
+        ISieveProcessor sieveProcessor) : IModelRepository
+    {
+        public async Task<Model> GetModelById(Guid modelId)
+        {
+            return await db.Models
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.Id == modelId);
+        }
+
+        public async Task<List<Model>> GetModels(SieveModel query)
+        {
+            var models = db
+                .Models
+                .AsNoTracking()
+                .AsQueryable();
+
+            return await sieveProcessor
+                .Apply(query, models)
+                .AsNoTracking()
+                .ToListAsync();
+        }
+
+        public async Task<int> GetModelsCount(SieveModel query)
+        {
+            var models = db
+                .Models
+                .AsNoTracking()
+                .AsQueryable();
+
+            return await sieveProcessor
+                .Apply(query, models, applyPagination: false)
+                .CountAsync();
+        }
+
+        public async Task<Model> CreateModel(Model model)
+        {
+            await db.AddAsync(model);
+            await db.SaveChangesAsync();
+
+            return model;
+        }
+
+        public async Task<Model> UpdateModel(Model updatedModel)
+        {
+            var model = await db.Models
+                .FirstOrDefaultAsync(c => c.Id == updatedModel.Id);
+
+            var entry = db.Entry(model);
+            entry.CurrentValues.SetValues(updatedModel);
+
+            await db.SaveChangesAsync();
+
+            return model;
+        }
+    }
+}
