d119064642593d71cf2a132464e5c967f0c5ddb7
JUKKarol <karolprzybylak2@gmail.com>
2024-01-15 14:07:35 +0100

    Add mappings for brand, model and generation

---
+++ api/Utilities/Mappings/BrandMappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using CarMarketAnalysis.DTOs.BrandDTOs;
+using CarMarketAnalysis.Entities;
+
+namespace CarMarketAnalysis.Utilities.Mappings
+{
+    public class BrandMappingProfile : Profile
+    {
+        public BrandMappingProfile()
+        {
+            CreateMap<BrandDisplayDto, Brand>().ReverseMap();
+            CreateMap<Brand, BrandDetalisDto>().ForMember(dest => dest.Models, opt => opt.MapFrom(src => src.Models));
+            CreateMap<BrandCreateDto, Brand>().ReverseMap();
+            CreateMap<BrandUpdateDto, Brand>().ReverseMap();
+        }
+    }
+}
---
+++ api/Utilities/Mappings/ModelMappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using CarMarketAnalysis.DTOs.ModelDTOs;
+using CarMarketAnalysis.Entities;
+
+namespace CarMarketAnalysis.Utilities.Mappings
+{
+    public class ModelMappingProfile : Profile
+    {
+        public ModelMappingProfile()
+        {
+            CreateMap<Model, ModelDetailsDto>().AfterMap((src, dest) => dest.BrandName = src.Brand.Name);
+            CreateMap<Model, ModelDisplayDto>().ReverseMap();
+            CreateMap<ModelCreateDto, Model>().ReverseMap();
+            CreateMap<ModelUpdateDto, Model>().ReverseMap();
+        }
+    }
+}
