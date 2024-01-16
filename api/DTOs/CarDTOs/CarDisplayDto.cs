ea387bb3b0c4f9e2e9d2366ce24ea914a37ddd85
JUKKarol <karolprzybylak2@gmail.com>
2024-01-16 19:02:01 +0100

    Add car dtos

--- api/DTOs/CarDTOs/CarDisplayDto.cs
+++ api/DTOs/CarDTOs/CarDisplayDto.cs
@@ -1,10 +1,9 @@
 ﻿using CarMarketAnalysis.DTOs.GenerationDTOs;
-using CarMarketAnalysis.Entities;
 using CarMarketAnalysis.Enums;
 
 namespace CarMarketAnalysis.DTOs.CarDTOs
 {
-    public class CarDetailsDto
+    public class CarDisplayDto
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
@@ -14,21 +13,9 @@ namespace CarMarketAnalysis.DTOs.CarDTOs
         public int YearOfProduction { get; set; }
         public int Mileage { get; set; }
         public int EngineSize { get; set; }
-        public int HorsePower { get; set; }
-        public bool AutomaticTransmission { get; set; }
         public FuelType FuelType { get; set; }
         public string Localization { get; set; }
         public string Slug { get; set; }
-        public bool ElectricSeat { get; set; }
-        public bool HeatedSeats { get; set; }
-        public bool HeatedBackSeats { get; set; }
-        public bool MassagedSeats { get; set; }
-        public bool FullElectricWindows { get; set; }
-        public bool Bluetooth { get; set; }
-        public bool CruiseControl { get; set; }
-        public bool Parktronic { get; set; }
-        public bool MultifunctionWheel { get; set; }
-        public bool HeatedWheel { get; set; }
 
         public DateTime CreatedAt { get; set; }
 
