67d2827ab6391ac3f9e90e8c44ce07f27f1267c7
JUKKarol <karolprzybylak2@gmail.com>
2024-01-17 22:00:47 +0100

    Add car services

---
+++ api/Services/CarService/CarService.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using CarMarketAnalysis.Data.Repositories.CarRepository;
+using CarMarketAnalysis.Data.Repositories.CarRepository;
+using CarMarketAnalysis.DTOs.CarDTOs;
+using CarMarketAnalysis.DTOs.SharedDTOs;
+using CarMarketAnalysis.Entities;
+using Sieve.Models;
+
+namespace CarMarketAnalysis.Services.CarService
+{
+    public class CarService(
+        ICarRepository carRepository, 
+        IMapper mapper) : ICarService
+    {
+        public async Task<CarDetailsDto> GetCarById(Guid carId)
+        {
+            var car = await carRepository.GetCarById(carId);
+            return mapper.Map<CarDetailsDto>(car);
+        }
+
+        public async Task<RespondListDto<CarDisplayDto>> GetCars(SieveModel query)
+        {
+            int pageSize = query.PageSize != null ? (int)query.PageSize : 40;
+
+            var cars = await carRepository.GetCars(query);
+            var carsDto = mapper.Map<List<CarDisplayDto>>(cars);
+
+            RespondListDto<CarDisplayDto> respondListDto = new();
+            respondListDto.Items = carsDto;
+            respondListDto.ItemsCount = await carRepository.GetCarsCount(query);
+            respondListDto.PagesCount = (int)Math.Ceiling((double)respondListDto.ItemsCount / pageSize);
+            return respondListDto;
+        }
+    }
+}
---
+++ api/Services/CarService/ICarService.cs
@@ -0,0 +1,12 @@
+﻿using CarMarketAnalysis.DTOs.SharedDTOs;
+using CarMarketAnalysis.DTOs.CarDTOs;
+using Sieve.Models;
+
+namespace CarMarketAnalysis.Services.CarService
+{
+    public interface ICarService
+    {
+        Task<CarDetailsDto> GetCarById(Guid carId);
+        Task<RespondListDto<CarDisplayDto>> GetCars(SieveModel query);
+    }
+}
