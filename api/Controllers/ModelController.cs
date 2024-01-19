2087cb12a4b49a86297a56e9bc926de176cf8377
JUKKarol <karolprzybylak2@gmail.com>
2024-01-19 21:19:45 +0100

    Add model controller

---
+++ api/Controllers/ModelController.cs
@@ -0,0 +1,32 @@
+﻿using CarMarketAnalysis.Services.BrandService;
+using CarMarketAnalysis.Services.ModelService;
+using Microsoft.AspNetCore.Mvc;
+using Sieve.Models;
+
+namespace CarMarketAnalysis.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ModelController(IModelService modelService) : ControllerBase
+
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetModelsByBrandName([FromQuery] SieveModel query)
+        {
+            return Ok(await modelService.GetModels(query));
+        }
+
+        [HttpGet("{modelId}")]
+        public async Task<IActionResult> GetModel(Guid modelId)
+        {
+            var model = await modelService.GetModelById(modelId);
+
+            if (model == null)
+            {
+                return NotFound("Model not found");
+            }
+
+            return Ok(model);
+        }
+    }
+}
