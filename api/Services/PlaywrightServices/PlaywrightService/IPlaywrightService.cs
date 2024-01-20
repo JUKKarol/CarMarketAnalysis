36ca2606ad82466b6be765a55e009afbb21a02eb
JUKKarol <karolprzybylak2@gmail.com>
2024-01-20 12:42:34 +0100

    Add playwright service with page count function

---
+++ api/Services/PlaywrightServices/PlaywrightService/IPlaywrightService.cs
@@ -0,0 +1,7 @@
+﻿namespace CarMarketAnalysis.Services.PlaywrightServices.PlaywrightService
+{
+    public interface IPlaywrightService
+    {
+        Task<string> GetPageCount();
+    }
+}
---
+++ api/Services/PlaywrightServices/PlaywrightService/PlaywrightService.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Routing;
+using Microsoft.Playwright;
+using System.Text.RegularExpressions;
+
+namespace CarMarketAnalysis.Services.PlaywrightServices.PlaywrightService
+{
+    public class PlaywrightService : IPlaywrightService
+    {
+        public async Task<string> GetPageCount()
+        {
+            using var playwright = await Playwright.CreateAsync();
+
+            await using var browser = await playwright.Chromium.LaunchAsync();
+
+            var page = await browser.NewPageAsync();
+            await page.GotoAsync("https://www.otomoto.pl/osobowe");
+
+            return await page.Locator("ul[class*='pagination-list'] li[data-testid='pagination-list-item']").Last.InnerTextAsync();
+        }
+    }
+}
