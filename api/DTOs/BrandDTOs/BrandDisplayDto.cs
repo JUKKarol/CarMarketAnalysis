7190e40b6fb1af73b143d000195e4893c855a7eb
JUKKarol <karolprzybylak2@gmail.com>
2024-01-23 05:51:48 +0100

    Remove data time from brand dto

--- api/DTOs/BrandDTOs/BrandDisplayDto.cs
+++ api/DTOs/BrandDTOs/BrandDisplayDto.cs
@@ -3,8 +3,6 @@
     public class BrandDisplayDto
     {
         public Guid Id { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set; }
         public string Name { get; set; }
     }
 }
